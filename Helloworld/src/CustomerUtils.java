c9f0c3070380562661fbf5742de024d2b39a7bfe
BaoNguyen <nguyenducbaokey@gmail.com>
2023-02-10 21:21:10 +0700

    init

---
+++ Helloworld/src/CustomerUtils.java
@@ -0,0 +1,11 @@
+/*
+ * (c) Copyright 2023 Fresher Academy. All Rights Reserved.
+ *
+ *	@author ADMIN
+ *	@date Feb 8, 2023
+ *	@version 1.0	
+ * */
+
+public class CustomerUtils {
+
+}
---
+++ Helloworld/src/Helloworld.java
@@ -0,0 +1,13 @@
+/*
+ * (c) Copyright ${year} Fresher Academy. All Rights Reserved.
+ *
+ *	@author ${user}
+ *	@date ${date}
+ *	@version 1.0	
+ * */
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
